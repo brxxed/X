607140bf67d72600bb46f608adce917129500a20
br0kE <167619285+br0k3V@users.noreply.github.com>
2024-04-25 18:58:46 +0800

    Upload all dev files [development]
    Upload all Visual Studio Development files to development branch

---
+++ brxxedXClient/Pages/HomePage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Timers;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using brxxedXClient;
+
+namespace brxxedXClient.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            timeLabel.Content = "Launched at " + DateTime.Now;
+
+
+        }
+
+        private void ButtonLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            LibraryPage libPage = new LibraryPage();
+            NavigationService.Navigate(libPage);
+        }
+
+        private void ButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDonate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ brxxedXClient/Pages/LibraryPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Timers;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using brxxedXClient;
+
+namespace brxxedXClient.Pages
+{
+    /// <summary>
+    /// Interaction logic for LibraryPage.xaml
+    /// </summary>
+    public partial class LibraryPage : Page
+    {
+        public LibraryPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void ButtonLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage libPage = new HomePage();
+            NavigationService.Navigate(libPage);
+        }
+
+        private void ButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDonate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
